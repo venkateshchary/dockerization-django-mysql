18a7a203a6238d9dde45729bac71a03c2df5d834
venkatesh <kiransai.palika@mobigesture.com>
2019-12-17 13:11:34 +0530

    adding the Docker related files.

--- mysiteproject/mysiteproject/settings.py
+++ mysiteproject/mysiteproject/settings.py
@@ -72,11 +72,14 @@ WSGI_APPLICATION = 'mysiteproject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'docker-db',
+        'USER': 'root',
+        'PASSWORD': 'venky@123',
+        'HOST': 'db',
+        'PORT': 3306,
     }
 }
 
